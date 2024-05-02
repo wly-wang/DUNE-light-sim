29d9c222c1ede5fabdec88c87fff353af31783ef
bwynne <bwynne@t3-mw2.ph.ed.ac.uk>
2024-05-02 11:31:43 +0100

    Use channel number as environment variable

--- merge/makeChannelMap.sh
+++ merge/makeChannelMap.sh
@@ -1,8 +1,11 @@
 SEARCH_STRING="Optical Channels positions:  "
 INPUT_FILE=../generate/process0/log
+INPUT_FILE=../../runWithWiresv6/run1/process0/log
 OUTPUT_FILE=protodune_optical_mapping.txt
 
-HEADER=`grep "${SEARCH_STRING}" ${INPUT_FILE}`
-CHANNEL_NUMBER=${HEADER#${SEARCH_STRING}}
-echo CHANNEL_NUMBER=${CHANNEL_NUMBER}: update src/main.cpp
+MAP_HEADER=`grep "${SEARCH_STRING}" ${INPUT_FILE}`
+export CHANNEL_NUMBER=${MAP_HEADER#${SEARCH_STRING}}
+echo CHANNEL_NUMBER=${CHANNEL_NUMBER}
+
 grep "${SEARCH_STRING}" -A ${CHANNEL_NUMBER} ${INPUT_FILE} | grep -v "${SEARCH_STRING}" > ${OUTPUT_FILE}
+echo Created map file ${OUTPUT_FILE}
