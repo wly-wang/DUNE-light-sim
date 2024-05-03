d15b67a4f695417a9050c333035c1b06534580ef
bwynne <bwynne@t3-mw2.ph.ed.ac.uk>
2024-05-03 13:20:18 +0100

    Use merged input

--- fit/run.sh
+++ fit/run.sh
@@ -1,5 +1,5 @@
-RUN=run3
-mkdir plots
-cp ../mergeWithWiresv6/CompiledMerger/${RUN}/output.${RUN}.root output.root
-root Semi_Mode_Gen_v2.C
-mv plots ${RUN}defaultPlusRange
+source makeChannelMap.sh
+
+make
+
+bin/FastOpticalFit ../merge/output.root
