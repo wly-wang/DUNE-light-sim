158cf608671daa77cf53953470a073e94d45672d
bwynne <bwynne@t3-mw2.ph.ed.ac.uk>
2024-05-03 13:20:31 +0100

    Found LARSOFT version with the optimised file size change

--- initialise.sh
+++ initialise.sh
@@ -1,6 +1,6 @@
 # Choose dune sw version
 ls /cvmfs/dune.opensciencegrid.org/products/dune/dunesw/
-export MY_DUNE_VERSION=v09_76_00
+export MY_DUNE_VERSION=v09_78_01
 
 # Env setup
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
