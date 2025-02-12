a46df55ef139048018f407aa7b32fc34a1c9b4f1
William Wang <wwang@dunegpvm11.fnal.gov>
2025-02-12 15:15:53 -0600

    updated dunecode version

--- initialise.sh
+++ initialise.sh
@@ -1,7 +1,7 @@
 # Choose dune sw version
 ls /cvmfs/dune.opensciencegrid.org/products/dune/dunesw/
-export MY_DUNE_VERSION=v09_78_01
+export MY_DUNE_VERSION=v10_03_01
 
 # Env setup
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-setup dunesw ${MY_DUNE_VERSION}d00 -q e20:prof
+setup dunesw ${MY_DUNE_VERSION}d00 -q e26:prof
